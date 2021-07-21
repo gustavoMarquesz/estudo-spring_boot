d8783a4f58a3ab532b9dfd1521a22e391f62503b
Gustavo Marques <gustavo99a@hotmail.com>
2021-07-21 19:21:40 -0300

    Projeto Criado com sucesso

---
+++ src/main/java/com/stormesteste/testandospringboot2/TestandoSpringboot2Application.java
@@ -0,0 +1,13 @@
+package com.stormesteste.testandospringboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestandoSpringboot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestandoSpringboot2Application.class, args);
+	}
+
+}
