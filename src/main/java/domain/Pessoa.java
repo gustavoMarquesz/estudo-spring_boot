5a4cb3f0bea487d9c0ab5b8caf1e0026c53e2ea2
Gustavo Marques <gustavo99a@hotmail.com>
2021-07-23 13:57:00 -0300

    Lombok adicionado em dependencias/pastas organizadas

---
+++ src/main/java/domain/Pessoa.java
@@ -0,0 +1,13 @@
+package domain;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.io.Serializable;
+
+public class Pessoa implements Serializable {
+
+    private Integer id;
+    private String nome;
+    private String email;
+
+
+}
---
+++ src/main/java/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+
+import domain.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository {
+}
