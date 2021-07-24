93d999e8e528341d0e091b7a3180bf12693a8718
Gustavo Marques <gustavo99a@hotmail.com>
2021-07-24 15:07:27 -0300

    Dependencia do thyneleaf adicionada, main ajustada

--- src/main/java/com/stormesteste/testandospringboot2/TestandoSpringboot2Application.java
+++ src/main/java/com/stormesteste/testandospringboot2/TestandoSpringboot2Application.java
@@ -1,39 +1,14 @@
 package com.stormesteste.testandospringboot2;
 
-
-import com.stormesteste.testandospringboot2.domain.Cota;
-import com.stormesteste.testandospringboot2.repositories.ConnetionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
+
 
 @SpringBootApplication
-public class TestandoSpringboot2Application implements CommandLineRunner {//command gerou o metodo abaixo
-	@Autowired
-	ConnetionRepository connetionRepository;//chamei o repository para fazer a conexao
-
-
+public class TestandoSpringboot2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestandoSpringboot2Application.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		Cota c1 = new Cota(null,"Gustavo","gustavo99a@hotmail.com",
-				"Churras","Asinha, Coxinha");
-		
-		Cota c2 = new Cota(null,"Guga","guga10@hotmail.com",
-				"Coxinha","Coxinha, coca");
-
-		connetionRepository.saveAll(Arrays.asList(c1,c2));
-
-
-
-	}
-
 }
--- src/main/java/com/stormesteste/testandospringboot2/domain/Cota.java
+++ src/main/java/com/stormesteste/testandospringboot2/domain/Cota.java
@@ -12,7 +12,7 @@ import javax.persistence.Id;
 import java.io.Serializable;
 
 @Entity
-
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
 
