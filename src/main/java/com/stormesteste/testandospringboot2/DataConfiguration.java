e80a6e70ac56733920c58ca5d5e4193eb3f6540b
Gustavo Marques <gustavo99a@hotmail.com>
2021-07-24 17:35:20 -0300

    Classe Conectora Data configuration adicionada

---
+++ src/main/java/com/stormesteste/testandospringboot2/DataConfiguration.java
@@ -0,0 +1,39 @@
+package com.stormesteste.testandospringboot2;
+//Classe de conexao com o bando de dados
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+public class DataConfiguration {
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3307/cotas");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        return dataSource;//acostumado com essas informações, agora somente de um jeito diferente
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){//adaptador para o JPA, como ja diz o nome
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+        adapter.setPrepareConnection(true);
+        return adapter;
+    }
+
+
+}
--- src/main/java/com/stormesteste/testandospringboot2/domain/Cota.java
+++ src/main/java/com/stormesteste/testandospringboot2/domain/Cota.java
@@ -11,12 +11,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 
-
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 
 public class Cota implements Serializable {
+    private static final long serialVersionUID = 1L;
 
 
     @Id
